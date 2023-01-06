ff7313dec10c5b4bebe95735fbc5a3a670022173
Armon Khosravi <akhosravi@brex.com>
2023-01-06 18:24:04 -0500

    Get working

--- graphql-field-finder.js
+++ graphql-field-finder.js
@@ -4,39 +4,44 @@
 // First, add a .graphqlconfig in your directory pointing to your schema
 // Then, run the script with:
 //
-// node graphql-field-finder.js Field.name
+// node graphql-field-finder.js "../relative/path/to/repo" projectName Field.name
 //
 // It will output a list of files and queries that contain the field you're
 // looking for:
 //
 // src/filename.js:123 MyFavoriteQuery
-//
-// Please try this out and comment below! To learn more, check out the
-// slides for the talk I gave about this here:
-// https://www.slideshare.net/sashko1/building-custom-graphql-tooling-for-your-team
 
-import { getGraphQLProjectConfig } from "graphql-config"
+import { loadConfigSync } from "graphql-config"
 import { exec } from "child_process"
 import { readFileSync } from "fs"
 import { parse as parseJS } from "@babel/parser"
-import traverse from "@babel/traverse"
+import _traverse from "@babel/traverse"
 import {
   parse as parseGraphQL,
   visit,
   visitWithTypeInfo,
   TypeInfo
 } from "graphql";
+const traverse = _traverse.default;
 
-const schema = getGraphQLProjectConfig().getSchema();
-const desiredTypeDotField = getDesiredTypeDotField();
+// repoLocation must be relative to where this is run from - I'm sure there's a better way to do this
+const repoLocation = process.argv[2];
+const projectName = process.argv[3];
+const fieldName = process.argv[4];
+
+const schema = loadConfigSync({
+  filepath: `${repoLocation}/.graphqlconfig`,
+}).getProject(projectName).getSchemaSync()
+
+const desiredTypeDotField = getDesiredTypeDotField(fieldName);
 
 // Do a grep to find which files have GraphQL queries in them so we don't
 // look through all JS in the whole app
-exec(`git grep -rl "gql" -- '*.js' '*.jsx'`, (error, stdout, stderr) => {
+exec(`cd ${repoLocation} && git grep -rl "gql" -- '*.ts' '*.tsx'`, (error, stdout, stderr) => {
   const files = stdout.split("\n").filter(filename => !!filename.length);
 
   const usages = files
-    .map(filename => findGraphQLUsagesInFile(filename, desiredTypeDotField))
+    .map(filename => findGraphQLUsagesInFile(`${repoLocation}/${filename}`, desiredTypeDotField))
     .flat();
 
   console.log(
@@ -52,7 +57,7 @@ function findGraphQLUsagesInFile(filename, typeDotField) {
   const fileContents = readFileSync(filename, { encoding: "utf-8" });
   const ast = parseJS(fileContents, {
     sourceType: "module",
-    plugins: ["jsx", "flow", "classProperties"]
+    plugins: ["jsx", "typescript"]
   });
 
   const graphqlStringNodes = [];
@@ -68,10 +73,15 @@ function findGraphQLUsagesInFile(filename, typeDotField) {
   graphqlStringNodes.forEach(jsNode => {
     const ast = parseGraphQL(jsNode.value.raw);
     const typeInfo = new TypeInfo(schema);
+    
     visit(
       ast,
       visitWithTypeInfo(typeInfo, {
         Field(graphqlNode) {
+          if (!typeInfo.getParentType()) {
+            return
+          }
+          
           const currentTypeDotField =
             typeInfo.getParentType().name + "." + graphqlNode.name.value;
 
@@ -90,8 +100,7 @@ function findGraphQLUsagesInFile(filename, typeDotField) {
 }
 
 // Get the first argument passed to this script and validate it a bit
-function getDesiredTypeDotField() {
-  const desiredTypeDotField = process.argv[2];
+function getDesiredTypeDotField(desiredTypeDotField) {
   if (!desiredTypeDotField) {
     throw new Error("Please supply a field name as Type.field");
   }
